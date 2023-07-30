8e499dc44e120351263778f239fbf186c088d7d2
john <jarrington08@gmail.com>
2023-07-30 16:13:19 -0500

    finished the optional final touches

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,5 +1,7 @@
+import classes from './MovieList.module.css';
+
 export default function BookList() {
-   let pageTitle = "Books to Read";
+   let pageTitle = "Books to Read:";
    let book1 = "https://m.media-amazon.com/images/I/41xYsSW6+IL._SY344_BO1,204,203,200_.jpg";
    let book2 = "https://m.media-amazon.com/images/I/51FMs3gcjhL._SY346_.jpg";
    let book3 = "https://m.media-amazon.com/images/I/51MXUznlr6L._SX329_BO1,204,203,200_.jpg";
@@ -7,11 +9,13 @@ export default function BookList() {
    let height = "350em";
 
    return (
-      <div>
-         <h3>{pageTitle}</h3>
-         <img src={book1} alt="Leslie Wolfe's newest release The Surgeon" width={width} height={height} />
-         <img src={book2} alt="James Patterson's newest release Obsessed" width={width} height={height} />
-         <img src={book3} alt="Johnny Joey Jones' newest release Unbroken Bonds of Battle" width={width} height={height} />
+      <div style={{backgroundColor:"lightgray"}}>
+         <h3 className={classes.movieHeading} style={{marginLeft:"24%"}}>{pageTitle}</h3>
+         <div>
+            <img src={book1} alt="Leslie Wolfe's newest release The Surgeon" width={width} height={height} />
+            <img src={book2} alt="James Patterson's newest release Obsessed" width={width} height={height} />
+            <img src={book3} alt="Johnny Joey Jones' newest release Unbroken Bonds of Battle" width={width} height={height} />
+         </div>
       </div>      
    );
 }
--- part1/exercises/src/components/HobbyLinks.js
+++ part1/exercises/src/components/HobbyLinks.js
@@ -1,9 +1,14 @@
+import classes from './MovieList.module.css';
+
 export default function HobbyLinks() {
     let hobbyLinks = ["www.minecraft.com", "www.beeradvocate.com"];
     return (
         <div>
-            <a href = {hobbyLinks[0]}>My favorite game</a>
-            <a href = {hobbyLinks[1]}>My favorite beverage</a>
+            <h3 className={classes.movieHeading}>My Hobbies:</h3>
+            <ul>
+                <li><a href = {hobbyLinks[0]}>My favorite game</a></li>
+                <li><a href = {hobbyLinks[1]}>My favorite beverage</a></li>
+            </ul>
         </div>
     )
 }
