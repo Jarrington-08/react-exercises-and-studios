a3bbe29c6b5d7ec324147a17912de551551b1366
john <jarrington08@gmail.com>
2023-07-30 01:43:11 -0500

    worked on chores list, books list, movies list

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,17 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books to Read";
+   let book1 = "https://m.media-amazon.com/images/I/41xYsSW6+IL._SY344_BO1,204,203,200_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/51FMs3gcjhL._SY346_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/51MXUznlr6L._SX329_BO1,204,203,200_.jpg";
+   let width = "25%";
+   let height = "350em";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Leslie Wolfe's newest release The Surgeon" width={width} height={height} />
+         <img src={book2} alt="James Patterson's newest release Obsessed" width={width} height={height} />
+         <img src={book3} alt="Johnny Joey Jones' newest release Unbroken Bonds of Battle" width={width} height={height} />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,20 @@
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   let heading = "My chores:";
+   let chore1 = "LC homework";
+   let chore2 = "Take out the trash";
+   let chore3 = "Clean car";
+   return (
+      <div>
+         <h3 className = {classes.choresHeading}>{heading}</h3>
+            <p>
+               <ol className = {classes.choresText}>
+                  <li>{chore1}</li>
+                  <li>{chore2}</li>
+                  <li>{chore3}</li>
+               </ol>
+            </p>
+      </div>
+   )
 }
