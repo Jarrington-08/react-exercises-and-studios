68b1c198c0b457e4811d0fa7cbfc6b4344aa3a12
john <jarrington08@gmail.com>
2023-08-08 23:30:22 -0500

    started working on react part 3 exercises

---
+++ part3/exercises/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HobbyIntroduction from "./components/Introductions";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Hobby: Video Games</h1>
+      <HobbyIntroduction />
+    </div>
+  );
+}
+
+export default App;
---
+++ part3/exercises/src/components/Introductions.js
@@ -0,0 +1,16 @@
+function HobbyIntroduction() {
+    return (
+        <>
+            <div>
+                <h2>Three Fun Facts about Video Games</h2>
+                <ol>
+                    <li>The first video game ever was made in 1958</li>
+                    <li>Minecraft is the most popular video game of all time</li>
+                    <li>Star Citizen was the most expensive game to develop</li>
+                </ol>
+            </div>
+        </>
+    )
+}
+
+export default HobbyIntroduction;
