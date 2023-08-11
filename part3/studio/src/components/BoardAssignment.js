1d38299a51442a5cdb066a22cc79da86010d6251
john <jarrington08@gmail.com>
2023-08-10 21:47:48 -0500

    finished React Part 3 studio

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,15 +1,31 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
-
+   const boards = [
+      {
+         label: "Southern Cooking",
+         value: "Southern Cooking"
+      },
+      {
+         label: "Creole Recipes",
+         value: "Creole Recipes"
+      },
+      {
+         label: "Louisiana",
+         value: "Louisiana"
+      },
+   ];
+   const [boardName, setBoardName] = useState("no boards yet!");
+   
    const handleChange = (event) => {
+      setBoardName(event.target.value);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+      {boards.map((board) => <option key={board.value}>{board.label}</option>)};
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,11 +1,11 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://kennethtemple.com/about";
+   let authorPhoto = "https://kennethtemple.com/wp-content/uploads/2018/12/ChefKENNETH-CONTACT.png";
+   let authorName = "Kenneth Temple";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt = "Chef Kenneth Temple" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
             <a href={authorLink}></a> 
@@ -15,7 +15,7 @@ const RecipeAuthor = () => {
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["smoked beef sausage","seasoning blend","6 oz. tomato paste","cayenne","rice"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -34,8 +34,8 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>CREOLE JAMBALAYA RECIPE</h1>
+            <p>Authentic Creole Jambalaya</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -47,7 +47,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://kennethtemple.com/wp-content/uploads/2019/08/20-minute-jambalaya-683x1024.jpg" alt="Creole Jambalaya" className="imageUpdates"/>
    );
 }
 
